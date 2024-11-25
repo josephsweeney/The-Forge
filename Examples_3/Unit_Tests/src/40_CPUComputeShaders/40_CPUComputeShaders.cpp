1dff02862fe2cea6a0b5ef28abf5f32c2127292d
Joe Sweeney <joe@joesweeney.org>
2024-11-25 18:55:58 -0500

    Add Profiling UI to profile tests
    Now run the tests every frame.

--- Examples_3/Unit_Tests/src/40_CPUComputeShaders/40_CPUComputeShaders.cpp
+++ Examples_3/Unit_Tests/src/40_CPUComputeShaders/40_CPUComputeShaders.cpp
@@ -6,19 +6,30 @@
  */
 
 #include "../../../../Common_3/Application/Interfaces/IApp.h"
+#include "../../../../Common_3/Application/Interfaces/IFont.h"
+#include "../../../../Common_3/Application/Interfaces/IProfiler.h"
+#include "../../../../Common_3/Application/Interfaces/IUI.h"
+#include "../../../../Common_3/Application/Interfaces/IScreenshot.h"
 #include "../../../../Common_3/Graphics/Interfaces/IGraphics.h"
 #include "../../../../Common_3/Resources/ResourceLoader/Interfaces/IResourceLoader.h"
 #include "../../../../Common_3/Utilities/Interfaces/ILog.h"
+#include "../../../../Common_3/Utilities/RingBuffer.h"
+
 #include "ISPC/test.comp.h"
 
 // Test parameters 
-static const uint32_t COMPUTE_TEST_WIDTH = 256;
-static const uint32_t COMPUTE_TEST_HEIGHT = 256;
+static const uint32_t COMPUTE_TEST_WIDTH = 4096;
+static const uint32_t COMPUTE_TEST_HEIGHT = 4096;
 
 struct ComputeTestData {
     uint32_t width;
     uint32_t height;
 };
+
+ProfileToken gCpuComputeToken;
+ProfileToken gGpuComputeToken;
+
+uint32_t gFontID = 0;
 
 class CPUComputeTest: public IApp 
 {
@@ -34,11 +45,21 @@ public:
             LOGF(LogLevel::eERROR, "Failed to initialize renderer");
             return false;
         }
+        ProfilerDesc profiler = {};
+        profiler.pRenderer = pRenderer;
+        initProfiler(&profiler);
 
         // Create compute queue...
         QueueDesc queueDesc = {};
         queueDesc.mType = QUEUE_TYPE_COMPUTE;
         initQueue(pRenderer, &queueDesc, &pQueue);
+
+        GpuCmdRingDesc cmdRingDesc = {};
+        cmdRingDesc.pQueue = pQueue;
+        cmdRingDesc.mPoolCount = gDataBufferCount;
+        cmdRingDesc.mCmdPerPoolCount = 1;
+        cmdRingDesc.mAddSyncPrimitives = true;
+        initGpuCmdRing(pRenderer, &cmdRingDesc, &mCmdRing);
 
         // Create a single command pool and command buffer
         CmdPoolDesc cmdPoolDesc = {};
@@ -81,6 +102,25 @@ public:
         data->width = COMPUTE_TEST_WIDTH;
         data->height = COMPUTE_TEST_HEIGHT;
 
+        initSemaphore(pRenderer, &pImageAcquiredSemaphore);
+
+        // Load fonts
+        FontDesc font = {};
+        font.pFontPath = "TitilliumText/TitilliumText-Bold.otf";
+        fntDefineFonts(&font, 1, &gFontID);
+
+        FontSystemDesc fontRenderDesc = {};
+        fontRenderDesc.pRenderer = pRenderer;
+        if (!initFontSystem(&fontRenderDesc))
+            return false; // report?
+
+        // Initialize Forge User Interface Rendering
+        UserInterfaceDesc uiRenderDesc = {};
+        uiRenderDesc.pRenderer = pRenderer;
+        initUserInterface(&uiRenderDesc);
+
+        initScreenshotCapturer(pRenderer, pQueue, GetName());
+
         return true;
     }
 
@@ -88,15 +128,24 @@ public:
     {
         waitQueueIdle(pQueue);
 
+        exitScreenshotCapturer();
+
         removeResource(pOutputBuffer);
         removeResource(pUniformBuffer);
 
+        exitProfiler();
+        exitUserInterface();
+        exitFontSystem();
+
+        exitSemaphore(pRenderer, pImageAcquiredSemaphore);
+        exitGpuCmdRing(pRenderer, &mCmdRing);
         exitCmd(pRenderer, pCmd);
         exitCmdPool(pRenderer, pCmdPool);
         exitQueue(pRenderer, pQueue);
         exitResourceLoaderInterface(pRenderer);
         exitRenderer(pRenderer);
         exitGPUConfiguration();
+        pRenderer = NULL;
     }
 
     bool Load(ReloadDesc* pReloadDesc) override 
@@ -106,11 +155,30 @@ public:
         createDescriptorSet();
         createPipeline();
         updateDescriptors();
+        loadProfilerUI(mSettings.mWidth, mSettings.mHeight);
 
-        // Run the compute test immediately after resource creation
-        runComputeTest();
-        runCPUComputeTest();
-        requestShutdown();
+        UIComponentDesc guiDesc = {};
+        guiDesc.mStartPosition = vec2(mSettings.mWidth * 0.01f, mSettings.mHeight * 0.2f);
+        uiAddComponent(GetName(), &guiDesc, &pGuiWindow);
+
+        if (!addSwapChain()) {
+            return false;
+        }
+
+        UserInterfaceLoadDesc uiLoad = {};
+        uiLoad.mColorFormat = pSwapChain->ppRenderTargets[0]->mFormat;
+        uiLoad.mHeight = mSettings.mHeight;
+        uiLoad.mWidth = mSettings.mWidth;
+        uiLoad.mLoadType = pReloadDesc->mType;
+        loadUserInterface(&uiLoad);
+
+        FontSystemLoadDesc fontLoad = {};
+        fontLoad.mColorFormat = pSwapChain->ppRenderTargets[0]->mFormat;
+        fontLoad.mHeight = mSettings.mHeight;
+        fontLoad.mWidth = mSettings.mWidth;
+        fontLoad.mLoadType = pReloadDesc->mType;
+        loadFontSystem(&fontLoad);
+
         return true;
     }
 
@@ -118,14 +186,123 @@ public:
     {
         waitQueueIdle(pQueue);
 
+        unloadFontSystem(pReloadDesc->mType);
+        unloadUserInterface(pReloadDesc->mType);
         removePipeline(pRenderer, pComputePipeline);
         removeDescriptorSet(pRenderer, pDescriptorSet);
         removeRootSignature(pRenderer, pRootSignature);
         removeShader(pRenderer, pComputeShader);
+
+        if (pReloadDesc->mType & (RELOAD_TYPE_RESIZE | RELOAD_TYPE_RENDERTARGET))
+        {
+            removeSwapChain(pRenderer, pSwapChain);
+
+            uiRemoveComponent(pGuiWindow);
+            unloadProfilerUI();
+        }
     }
 
-    void Update(float deltaTime) override {}
-    void Draw() override {}
+    void Update(float deltaTime) override
+    {
+        PROFILER_SET_CPU_SCOPE("Cpu Profile", "update", 0x222222);
+
+        if (!uiIsFocused())
+        {
+            if (inputGetValue(0, CUSTOM_TOGGLE_FULLSCREEN))
+            {
+                toggleFullscreen(pWindow);
+            }
+            if (inputGetValue(0, CUSTOM_TOGGLE_UI))
+            {
+                uiToggleActive();
+            }
+            if (inputGetValue(0, CUSTOM_DUMP_PROFILE))
+            {
+                dumpProfileData(GetName());
+            }
+            if (inputGetValue(0, CUSTOM_EXIT))
+            {
+                requestShutdown();
+            }
+        }
+
+        runComputeTest();
+        runCPUComputeTest();
+    }
+
+    void Draw() override
+    {
+        if ((bool)pSwapChain->mEnableVsync != mSettings.mVSyncEnabled)
+        {
+            waitQueueIdle(pQueue);
+            ::toggleVSync(pRenderer, &pSwapChain);
+        }
+
+        PROFILER_SET_CPU_SCOPE("Cpu Profile", "draw", 0xffffff);
+
+        uint32_t swapchainImageIndex;
+        acquireNextImage(pRenderer, pSwapChain, pImageAcquiredSemaphore, NULL, &swapchainImageIndex);
+
+        GpuCmdRingElement elem = getNextGpuCmdRingElement(&mCmdRing, true, 1);
+        FenceStatus       fenceStatus = {};
+        getFenceStatus(pRenderer, elem.pFence, &fenceStatus);
+        if (fenceStatus == FENCE_STATUS_INCOMPLETE)
+            waitForFences(pRenderer, 1, &elem.pFence);
+
+        resetCmdPool(pRenderer, elem.pCmdPool);
+
+        Cmd* pCmd = elem.pCmds[0];
+        beginCmd(pCmd);
+
+        RenderTarget* pRenderTarget = pSwapChain->ppRenderTargets[swapchainImageIndex];
+
+        BindRenderTargetsDesc bindRenderTargets = {};
+        bindRenderTargets.mRenderTargetCount = 1;
+        bindRenderTargets.mRenderTargets[0] = { pRenderTarget, LOAD_ACTION_CLEAR };
+        cmdBindRenderTargets(pCmd, &bindRenderTargets);
+        cmdSetViewport(pCmd, 0.0f, 0.0f, (float)mSettings.mWidth, (float)mSettings.mHeight, 0.0f, 1.0f);
+        cmdSetScissor(pCmd, 0, 0, mSettings.mWidth, mSettings.mHeight);
+
+        cmdBeginDebugMarker(pCmd, 0, 1, 0, "Draw UI");
+
+        FontDrawDesc frameTimeDraw;
+        frameTimeDraw.mFontColor = 0xff0080ff;
+        frameTimeDraw.mFontSize = 18.0f;
+        frameTimeDraw.mFontID = gFontID;
+        cmdDrawCpuProfile(pCmd, float2(8.0f, 15.0f), &frameTimeDraw);
+
+        cmdDrawUserInterface(pCmd);
+        cmdBindRenderTargets(pCmd, NULL);
+        cmdEndDebugMarker(pCmd);
+
+        RenderTargetBarrier presentBarrier = { pRenderTarget, RESOURCE_STATE_RENDER_TARGET, RESOURCE_STATE_PRESENT };
+        cmdResourceBarrier(pCmd, 0, NULL, 0, NULL, 1, &presentBarrier);
+
+        endCmd(pCmd);
+
+        FlushResourceUpdateDesc flushUpdateDesc = {};
+        flushUpdateDesc.mNodeIndex = 0;
+        flushResourceUpdates(&flushUpdateDesc);
+        Semaphore* waitSemaphores[2] = { flushUpdateDesc.pOutSubmittedSemaphore, pImageAcquiredSemaphore };
+
+        QueueSubmitDesc submitDesc = {};
+        submitDesc.mCmdCount = 1;
+        submitDesc.mSignalSemaphoreCount = 1;
+        submitDesc.mWaitSemaphoreCount = TF_ARRAY_COUNT(waitSemaphores);
+        submitDesc.ppCmds = &pCmd;
+        submitDesc.ppSignalSemaphores = &elem.pSemaphore;
+        submitDesc.ppWaitSemaphores = waitSemaphores;
+        submitDesc.pSignalFence = elem.pFence;
+        queueSubmit(pQueue, &submitDesc);
+        QueuePresentDesc presentDesc = {};
+        presentDesc.mIndex = (uint8_t)swapchainImageIndex;
+        presentDesc.mWaitSemaphoreCount = 1;
+        presentDesc.ppWaitSemaphores = &elem.pSemaphore;
+        presentDesc.pSwapChain = pSwapChain;
+        presentDesc.mSubmitDone = true;
+        queuePresent(pQueue, &presentDesc);
+        flipProfiler();
+    }
 
     const char* GetName() override { return "40_CPUComputeShaders"; }
 
@@ -173,6 +350,7 @@ private:
     
     void runComputeTest()
     {
+        PROFILER_SET_CPU_SCOPE("Tests", "GPU", 0x222222);
         resetCmdPool(pRenderer, pCmdPool);
         beginCmd(pCmd);
 
@@ -205,6 +383,7 @@ private:
 
     void runCPUComputeTest()
     {
+        PROFILER_SET_CPU_SCOPE("Tests", "CPU", 0x222222);
         uint32_t groupSizeX = (COMPUTE_TEST_WIDTH + 15) / 16;  // Assuming 16x16 thread groups
         uint32_t groupSizeY = (COMPUTE_TEST_HEIGHT + 15) / 16;
         const uint32_t numElements = COMPUTE_TEST_WIDTH * COMPUTE_TEST_HEIGHT;
@@ -218,6 +397,7 @@ private:
         // settings.height = COMPUTE_TEST_HEIGHT;
 
         ispc::CS_MAIN(outputBuffer, settings, groupSizeX, groupSizeY, 1);
+        
 
         LOGF(LogLevel::eINFO, "CPU Compute Test Results:");
         for (uint32_t i = 0; i < 10 && i < COMPUTE_TEST_WIDTH * COMPUTE_TEST_HEIGHT; i++) {
@@ -226,21 +406,40 @@ private:
         free(outputBuffer);
     }
 
-    // Core renderer objects
-    Renderer* pRenderer = nullptr;
-    Queue* pQueue = nullptr;
-    CmdPool* pCmdPool = nullptr;
-    Cmd* pCmd = nullptr;
+    bool addSwapChain()
+    {
+        SwapChainDesc swapChainDesc = {};
+        swapChainDesc.mColorClearValue = {};
+        swapChainDesc.mEnableVsync = mSettings.mVSyncEnabled;
+        swapChainDesc.mWidth = mSettings.mWidth;
+        swapChainDesc.mHeight = mSettings.mHeight;
+        swapChainDesc.mImageCount = getRecommendedSwapchainImageCount(pRenderer, &pWindow->handle);
+        swapChainDesc.ppPresentQueues = &pQueue;
+        swapChainDesc.mPresentQueueCount = 1;
+        swapChainDesc.mWindowHandle = pWindow->handle;
+        swapChainDesc.mColorFormat = getSupportedSwapchainFormat(pRenderer, &swapChainDesc, COLOR_SPACE_SDR_SRGB);
+        swapChainDesc.mColorSpace = COLOR_SPACE_SDR_SRGB;
+        ::addSwapChain(pRenderer, &swapChainDesc, &pSwapChain);
 
-    // Resources
-    Buffer* pOutputBuffer = nullptr;
-    Buffer* pUniformBuffer = nullptr;
+        return pSwapChain != NULL;
+    }
 
-    // Pipeline objects  
-    Shader* pComputeShader = nullptr;
-    RootSignature* pRootSignature = nullptr;
-    DescriptorSet* pDescriptorSet = nullptr;
-    Pipeline* pComputePipeline = nullptr;
+    static const uint32_t gDataBufferCount = 2;
+    
+    Renderer* pRenderer = NULL;
+    Queue* pQueue = NULL;
+    GpuCmdRing mCmdRing = {};
+    CmdPool* pCmdPool = NULL;
+    Cmd* pCmd = NULL;
+    SwapChain* pSwapChain = NULL;
+    Buffer* pOutputBuffer = NULL;
+    Buffer* pUniformBuffer = NULL;
+    Shader* pComputeShader = NULL;
+    RootSignature* pRootSignature = NULL;
+    DescriptorSet* pDescriptorSet = NULL;
+    Pipeline* pComputePipeline = NULL;
+    Semaphore* pImageAcquiredSemaphore = NULL;
+    UIComponent* pGuiWindow = NULL;
 };
 
 DEFINE_APPLICATION_MAIN(CPUComputeTest)
